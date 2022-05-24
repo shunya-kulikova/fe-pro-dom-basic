1f57705a73180aa8cf03372036599f2fb171b008
unknown <a.kulikova.design@gmail.com>
2022-05-24 04:35:57 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    const getLi = document.querySelectorAll('li:nth-child(odd)')
+    for (const li of getLi) {
+        li.style.backgroundColor = 'red'
+    }
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+    let getLi = document.querySelector('li')
+    while (getLi) {
+
+        if (getLi.matches('.likedItem')) {
+            getLi.style.backgroundColor = 'blue' 
+        }
+        getLi = getLi.nextElementSibling;
+        
+    }
+};
